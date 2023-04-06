ba6a715fd1e18c5218b5468e2b7535c2d833190d
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-07 01:22:01 +0300

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'brown', 'angry', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'white', 'lazy', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'M', 'gray', 'sleepy', 'False');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'F', 'white', 'hungry', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'black', 'thirsty', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'black', 'lovely', 'False');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'brown', 'grumpy', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, 'M', 'brown', 'old', 'True');
